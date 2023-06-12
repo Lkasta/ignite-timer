2f83e536212c64936d869ea28176a8b61fe0c714
Luiz Karpinski <lkastabackup@gmail.com>
2023-06-12 13:00:10 -0300

    update eslint
    add eslint para padrão de codigo

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,16 +1,16 @@
-import styled, { css } from "styled-components"
+import styled, { css } from 'styled-components'
 
-export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
 interface ButtonContainerProps {
-  variant: ButtonVariant;
+  variant: ButtonVariant
 }
 
 const buttonVariants = {
   primary: 'purple',
   secondary: 'orange',
   danger: 'red',
-  success: 'green'
+  success: 'green',
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
@@ -19,13 +19,13 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   margin: 8px;
   border-radius: 8px;
   border: 0;
-  
-  color: ${props => props.theme.white};
-  background-color: ${props => props.theme["green-500"]};
 
-  /* ${props => {
+  color: ${(props) => props.theme.white};
+  background-color: ${(props) => props.theme['green-500']};
+
+  /* ${(props) => {
     return css`
-      background-color: ${buttonVariants[props.variant]}
+      background-color: ${buttonVariants[props.variant]};
     `
   }} */
-` 
+`
